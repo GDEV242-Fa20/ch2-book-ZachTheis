0c26e81611897a45ab57e36f43229242ed30eb06
ZachTheis <zach.theis+zacharywstheis@gmail.com>
2020-09-21 20:21:36 -0400

    refNumber
    Added refNumber field and related accessor and mutator.
    
    Altered printDetails to include refNumber.

--- Book.java
+++ Book.java
@@ -12,6 +12,7 @@ class Book
     private String author;
     private String title;
     private int pages;
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
@@ -22,6 +23,7 @@ class Book
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
     }
 
     // Add the methods here ...
@@ -55,6 +57,24 @@ class Book
     }
     
     /**
+     * Provides access to the book's Reference Number.
+     * This satisfies part of 2.88
+     */
+    public String getRefNumber()
+    {
+        return refNumber;
+    }
+    
+    /**
+     * Stores the book's reference number
+     * This satisfies part of 2.88
+     */
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+    
+    /**
      * Tells the user the name of the book's author
      * This satisfies part of 2.84
      */
@@ -73,14 +93,22 @@ class Book
     }
     
     /**
-     * Tells the user the author, title, and page count of the book.
-     * This satisfies 2.87
+     * Tells the user the author, title, page count, and reference number of the book.
+     * This satisfies 2.87 and 2.89
      */
     public void printDetails()
     {
         System.out.println("Title: " + title);
         System.out.println("Author: " + author);
         System.out.println("Pages: " + pages);
+        if(refNumber.length() > 0)
+        {
+            System.out.println("Reference #: " + refNumber);
+        }
+        else
+        {
+            System.out.println("Reference #: zzz");
+        }
     }
     
     
